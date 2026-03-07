ba5c279bbdb3974014f8bf314843ccb5469d6bef
dege.spr <dgspr.dev@gmail.com>
2026-03-06 23:23:13 -0300

    commit atrasado rs

--- src/main/java/com/software/docifestas/controller/ProdutoController.java
+++ src/main/java/com/software/docifestas/controller/ProdutoController.java
@@ -3,10 +3,9 @@ package com.software.docifestas.controller;
 import com.software.docifestas.model.Produto;
 import com.software.docifestas.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -17,4 +16,9 @@ public class ProdutoController {
 
     @PostMapping
     public Produto criarProduto(@RequestBody Produto produto) {return myBrain.salvar(produto);}
+
+    @GetMapping
+    public List<Produto> listarTudo() {
+        return myBrain.listarTodos();
+    }
 }
